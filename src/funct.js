1e45e9b1f56dd7eb3edaee032d9805484ddf44f2
MenendezON <menendezon@gmail.com>
2023-05-16 21:37:06 +0000

    Fix issue from the funct.js file

--- src/funct.js
+++ src/funct.js
@@ -1,14 +1,14 @@
 /* eslint-disable-next-line import/no-cycle */
 import dataLoading from './index.js';
 
-const tasks = JSON.parse(localStorage.getItem('datas'));
-
+const tasks = JSON.parse(localStorage.getItem('datas')) ?? [];
 const validateForm = (event) => {
   event.preventDefault();
   const task = document.getElementById('newTask');
+  const taskLength = tasks.length;
   if (task && task.value !== '') {
     const newTask = {
-      index: tasks.length,
+      index: taskLength,
       completed: false,
       description: task && task.value,
     };
